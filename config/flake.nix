237ddd3fa68c34d2848cc603dc3ae79d911d390f
Francesco Tabaro <francesco.tabaro@gmail.com>
2024-03-10 17:44:36 +0100

    Move to Flake

--- config/flake.nix
+++ config/flake.nix
@@ -2,31 +2,38 @@
   description = "My personal NixOS/Home-Manager Flake config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+    nixpkgs.url = github:nixos/nixpkgs?ref=nixos-23.11;
 
+    home-manager = {
+      url = github:nix-community/home-manager/release-23.11;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {self, nixpkgs, home-manager}: {
-
-    nixosConfigurations.norbert = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager 
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPkgs = true;
-          home-manager.users.francesc = {
-            imports = [
-              ./home.nix
-            ];
-          };
-        }
-      ];
+  outputs = {self, nixpkgs, home-manager}: 
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system
+      config.allowUnfree = true;
     };
-  };
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      norbert = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPkgs = true;
+            home-manager.users.francesco = {
+              imports = [
+                ./home.nix
+              ];
+            };
+          }
+        ];
+      };
+    };
 }
