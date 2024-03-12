65ddefe87f517c9d9db843be461237f287de5a51
Francesco Tabaro <francesco.tabaro@gmail.com>
2024-03-12 20:46:40 +0100

    format and lint

--- config/flake.nix
+++ config/flake.nix
@@ -2,39 +2,45 @@
   description = "My personal NixOS/Home-Manager Flake config";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs?ref=nixos-23.11;
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
 
     home-manager = {
-      url = github:nix-community/home-manager/release-23.11;
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {self, nixpkgs, home-manager}: 
-  let 
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    lib = nixpkgs.lib;
-  in {
-    nixosConfigurations = {
-      norbert = lib.nixosSystem {
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
         inherit system;
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.francesco = {
-              imports = [
-                ./home.nix
-              ];
-            };
-          }
-        ];
+        config.allowUnfree = true;
+      };
+      lib = import nixpkgs {
+        inherit lib;
+      };
+    in
+    {
+      nixosConfigurations = {
+        norbert = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.francesco = {
+                  imports = [
+                    ./home.nix
+                  ];
+                };
+              };
+            }
+          ];
+        };
       };
     };
-  };
 }
