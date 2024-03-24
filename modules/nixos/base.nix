ce7706994ee5d96f9c9d7c97f1a8c60838c1f03a
Francesco Tabaro <francesco.tabaro@gmail.com>
2024-03-24 15:23:42 +0100

    fix pipewire

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -140,19 +140,20 @@
       interval = "daily 12:00";
       pruneBindMounts = true;
     };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
   };
 
   xserver = {
     enable = true;
 
-  };
-
-  pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
   };
 
   hardware.pulseaudio.enable = false;
