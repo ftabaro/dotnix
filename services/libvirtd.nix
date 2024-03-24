926a455f4494e65dab4926f5c8df6bf32bcb144a
Francesco Tabaro <francesco.tabaro@gmail.com>
2024-03-24 19:02:56 +0100

    add kernel module automatically

--- services/libvirtd.nix
+++ services/libvirtd.nix
@@ -1,29 +1,51 @@
-{ inputs, config, pkgs, ... }:
+{ inputs, config, pkgs, lib, ... }:
 
 let
-  baseConfig = import ./../modules/nixos/base.nix { 
+  baseConfig = import ./../modules/nixos/base.nix {
     inherit inputs config pkgs;
-};
-in
-{
-  # enable virtualisation
-  virtualisation.libvirtd = with pkgs; {
-    enable = true;
-    qemu = {
-      package = qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd
-        ];
-      };
-    };
   };
 
-  users.users.francesco.extraGroups = baseConfig.users.users.francesco.extraGroups ++ [ "libvirtd" ];
+  kvmModule = [ "kvm-amd" ];
+
+  # Add kvm-amd kernel module to the kernelModules array.
+  # If the array is not defined, define it with the kvmModule array.
+  # Otherwise, append kvmModule to config.boot.kernelModules.
+  newConfig =
+    if config.boot.kernelModules != null then
+      {
+        boot = config.boot // {
+          kernelModules = config.boot.kernelModules ++ kvmModule;
+        };
+      }
+    else {
+      boot = config.boot // {
+        kernelModules = kvmModule;
+      };
+    };
+in
+{
+  # Inherit the newConfig object as new config
+  inherit (config // { boot = newConfig; });
+
+  # enable virtualisation
+  config.virtualisation.libvirtd = with pkgs;
+    {
+      enable = true;
+      qemu = {
+        package = qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (OVMF.override {
+              secureBoot = true;
+              tpmSupport = true;
+            }).fd
+          ];
+        };
+      };
+    };
+
+  config.users.users.francesco.extraGroups = baseConfig.users.users.francesco.extraGroups ++ [ "libvirtd" ];
 }
