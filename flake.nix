9006013c51e1792bf08a929f8a1010beca585514
Francesco Tabaro <francesco.tabaro@gmail.com>
2024-03-24 15:01:52 +0100

    semicolon

--- flake.nix
+++ flake.nix
@@ -24,42 +24,45 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      
+
       formatter = forAllSystems (system: nixpkgs.legacyPacakges.${system}.alejandra);
 
       # norbert
       nixosConfigurations = {
-        norbert = nixpkgs.lib.nixosSystem {
-          specialAgs = {
-            inherit inputs outputs};
-              modules= [
-            ./machines/norbert/configuration.nix
+        norbert = nixpkgs.lib.nixosSystem
+          {
+            specialAgs = {
+              inherit inputs outputs;
+            };
+            modules = [
+              ./machines/norbert/configuration.nix
             ];
-            }
-            };
+          }
+          };
 
-              #     ./configuration.nix
-              #     home-manager.nixosModules.home-manager
-              #     {
-              #       home-manager = {
-              #         useGlobalPkgs = true;
-              #         useUserPackages = true;
-              #         users.francesco = {
-              #           imports = [
-              #             ./home.nix
-              #           ];
-              #         };
-              #       };
-              #     }
-              #   ];
-              # };
+        #     ./configuration.nix
+        #     home-manager.nixosModules.home-manager
+        #     {
+        #       home-manager = {
+        #         useGlobalPkgs = true;
+        #         useUserPackages = true;
+        #         users.francesco = {
+        #           imports = [
+        #             ./home.nix
+        #           ];
+        #         };
+        #       };
+        #     }
+        #   ];
+        # };
 
-              nixosConfigurations= {
-              pc-boulard07= nixpkgs. lib. nixosSystem{
-              specialAgs= {inherit inputs outputs};
-              modules= [ ./machines/pc-boulard07/configuration.nix ];
-            };
-            };
-            };
-            }
-            }
+        nixosConfigurations = {
+          pc-boulard07 = nixpkgs.lib.nixosSystem {
+            specialAgs = {
+              inherit inputs outputs};
+                modules= [ ./machines/pc-boulard07/configuration.nix ];
+              };
+              };
+              };
+              }
+              }
