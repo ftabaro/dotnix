c772908aaee6589c9c4b106b71441076c956662d
Francesco Tabaro <francesco.tabaro@gmail.com>
2024-03-24 19:02:28 +0100

    lint

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -18,54 +18,54 @@
         host = "gitlab.com";
         hostname = "gitlab.com";
         user = "git";
-        identityFile = "~/.ssh/id_gitlab"
-          };
+        identityFile = "~/.ssh/id_gitlab";
+      };
 
-        "git.embl.de" = {
-          host = "git.embl.de";
-          hostname = "git.embl.de";
-          identityFile = "~/.ssh/id_gitlab_embl";
-          forwardX11 = "no";
-          forwardX11Trusted = "no";
-        };
+      "git.embl.de" = {
+        host = "git.embl.de";
+        hostname = "git.embl.de";
+        identityFile = "~/.ssh/id_gitlab_embl";
+        forwardX11 = "no";
+        forwardX11Trusted = "no";
+      };
 
-        "seneca" = {
-          host = "seneca";
-          hostname = "seneca.embl.de";
-          user = "tabaro";
-          identityFile = "~/.ssh/id_embl";
-        };
+      "seneca" = {
+        host = "seneca";
+        hostname = "seneca.embl.de";
+        user = "tabaro";
+        identityFile = "~/.ssh/id_embl";
+      };
 
-        "login01" = {
-          host = "login01";
-          hostname = "login01.cluster.embl.de";
-          user = "tabaro";
-          identityFile = "~/.ssh/id_embl";
-        };
+      "login01" = {
+        host = "login01";
+        hostname = "login01.cluster.embl.de";
+        user = "tabaro";
+        identityFile = "~/.ssh/id_embl";
+      };
 
-        "login02" = {
-          host = "login02";
-          hostname = "login02.cluster.embl.de";
-          user = "tabaro";
-          identityFile = "~/.ssh/id_embl";
-        };
+      "login02" = {
+        host = "login02";
+        hostname = "login02.cluster.embl.de";
+        user = "tabaro";
+        identityFile = "~/.ssh/id_embl";
+      };
 
-        "login03" = {
-          host = "login03";
-          hostname = "login03.cluster.embl.de";
-          user = "tabaro";
-          identityFile = "~/.ssh/id_embl";
-        };
+      "login03" = {
+        host = "login03";
+        hostname = "login03.cluster.embl.de";
+        user = "tabaro";
+        identityFile = "~/.ssh/id_embl";
+      };
 
-        "login04" = {
-          host = "login04";
-          hostname = "login04.cluster.embl.de";
-          user = "tabaro";
-          ientityFile = "~/.ssh/id_embl";
-        };
-
+      "login04" = {
+        host = "login04";
+        hostname = "login04.cluster.embl.de";
+        user = "tabaro";
+        ientityFile = "~/.ssh/id_embl";
       };
 
     };
 
-  }
+  };
+
+}
